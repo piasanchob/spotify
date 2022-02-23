e4d52c7c2e552411976ffbd05ccd17952e316563
GabyGV <56004250+GabyGV@users.noreply.github.com>
2022-02-23 16:31:09 -0600

    create

--- AppSpotify/app/src/main/java/com/example/appspotify/data/entities/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appspotify.data.entities
-
-data class Song (
-    val mediaID: String = "",
-    val title: String = "",
-    val subtitle: String = "",
-    val songUrl: String = "",
-    val imageUrl: String = ""
-)
